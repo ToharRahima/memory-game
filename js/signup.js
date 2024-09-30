441e4030bb75f9bd074184cf12a6755176e7273d
Tohar <toharrahima@hilma.tech>
2024-09-30 16:11:34 +0300

    memory game

---
+++ js/signup.js
@@ -0,0 +1,8 @@
+const inputsColection = document.getElementsByClassName("inputs");
+inputsArr = Array.from(inputsColection);
+console.log( inputsArr );
+
+//אובייקט של שם ותוכן
+//לעשות מזה מערך של האובייקטים
+//את המערך של האובייקטים נכניס ללוקל סטורג
+//ההכנסה תתבצע בעת הלחיצה על כפתור הסבמיט
